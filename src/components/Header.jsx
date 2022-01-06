bc87bb8005a6004197c576311be3cbc9a0225260
maopos <mauricioposadamurillo@gmail.com>
2022-01-06 18:04:38 -0500

    Componentes y tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <h1 className="font-bold text-2xl text-center md:w-1/3 mx-auto">
+      Animal tracking at{" "}
+      <span className="text-green-700 md:text-5xl text-4xl">mydearpet.com</span>{" "}
+    </h1>
+  );
+};
+
+export default Header;
